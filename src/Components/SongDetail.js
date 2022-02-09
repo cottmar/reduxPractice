4a543f16330eb4e1d00cc9506209f14b10861141
cottmar <cara.ottmar@gmail.com>
2022-02-08 17:14:31 -0800

    small redux practice project

---
+++ src/Components/SongDetail.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from 'react-redux';
+
+const SongDetail = ({song}) => {
+    console.log(song, 'what are props in song detail')
+    if (!song) {
+        return <div>Select a song</div>
+    }
+    return (
+        <div>
+            <h3>Details For: </h3>
+            <p>
+                Title: {song.title}
+                Duration: {song.duration}
+            </p>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong }
+}
+
+export default connect(mapStateToProps)(SongDetail);
